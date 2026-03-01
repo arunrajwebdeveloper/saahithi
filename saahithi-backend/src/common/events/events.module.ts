c954a81de1bfa35f18ff56e4d2e4bb0122155ffa
Arun Raj <arunrajwebdeveloper@gmail.com>
2026-03-01 15:09:18 +0530

    fix

--- saahithi-backend/src/common/events/events.module.ts
+++ saahithi-backend/src/common/events/events.module.ts
@@ -1,21 +1,45 @@
-import { forwardRef, Global, Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { EventEmitterModule } from '@nestjs/event-emitter';
-import { GatewayModule } from '@/common/gateway/gateway.module';
 import { UserEvents } from './user.events';
 import { ContentEvents } from './content.events';
 import { AdminListener } from './admin.listener';
 import { AdminModule } from '@/modules/admin/admin.module';
 
-@Global()
+@Global() // it will make this module globally available
 @Module({
   imports: [
     EventEmitterModule.forRoot({
       wildcard: true, // allows events like "user.*"
     }),
-    GatewayModule,
-    forwardRef(() => AdminModule),
+    AdminModule,
+    // forwardRef(() => AdminModule), // use for circular dependency situations
   ],
   providers: [UserEvents, ContentEvents, AdminListener],
-  exports: [EventEmitterModule, ContentEvents, UserEvents],
+  exports: [EventEmitterModule, ContentEvents, UserEvents], // must export those files to use other modules like service files, so it can use straightly use in other services without import module, only need to import service
 })
 export class EventsModule {}
+
+// First, what forwardRef() actually does
+
+// In NestJS, modules and providers are instantiated in a dependency graph.
+// When two modules or services depend on each other, NestJS can’t decide which to create first.
+// forwardRef() tells Nest:
+// “This dependency will exist later — resolve it lazily.”
+// So it’s used to break circular dependencies.
+
+// Common circular dependency situations
+// Module ↔ Module circular imports
+// Eg:
+/*
+// admin.module.ts
+@Module({
+  imports: [EventsModule],
+})
+export class AdminModule {}
+
+// events.module.ts
+@Module({
+  imports: [AdminModule],
+})
+export class EventsModule {}
+*/
--- saahithi-backend/src/modules/admin/admin.module.ts
+++ saahithi-backend/src/modules/admin/admin.module.ts
@@ -1,18 +1,11 @@
-import { forwardRef, Module } from '@nestjs/common';
+import { Module } from '@nestjs/common';
 import { AdminService } from './admin.service';
 import { AdminController } from './admin.controller';
 import { ContentModule } from '../content/content.module';
-import { GatewayModule } from '@/common/gateway/gateway.module';
-import { EventsModule } from '@/common/events/events.module';
 import { UsersModule } from '../users/users.module';
 
 @Module({
-  imports: [
-    forwardRef(() => EventsModule),
-    GatewayModule,
-    UsersModule,
-    ContentModule,
-  ],
+  imports: [UsersModule, ContentModule],
   controllers: [AdminController],
   providers: [AdminService],
   exports: [AdminService],
