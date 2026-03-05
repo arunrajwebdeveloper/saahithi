a6cfbdd71b5269d2220ff4511753dea46db6524e
Arun Raj <arunrajwebdeveloper@gmail.com>
2026-03-05 13:01:04 +0530

    spinner

--- saahithi-admin/src/components/AppSpinner.tsx
+++ saahithi-admin/src/components/AppSpinner.tsx
@@ -2,7 +2,7 @@ import CircleSpinner from "./common/CircleSpinner";
 
 export const AppSpinner = () => {
   return (
-    <div className="w-full h-dvh flex fixed top-0 left-0 z-6000 bg-slate-100">
+    <div className="w-full h-dvh flex fixed top-0 left-0 z-6000 bg-card">
       <CircleSpinner size={24} className="m-auto text-blue-500" />
     </div>
   );
