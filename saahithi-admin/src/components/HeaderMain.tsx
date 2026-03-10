7bda5c472f1b7f3b2e3c0c6e864fa728ed963f2a
Arun Raj <arunrajwebdeveloper@gmail.com>
2026-03-10 10:59:01 +0530

    fix

--- saahithi-admin/src/components/HeaderMain.tsx
+++ saahithi-admin/src/components/HeaderMain.tsx
@@ -10,9 +10,11 @@ const HeaderMain = () => {
 
   return (
     <header className="w-full h-16 sticky top-0 z-40 bg-card border-b border-b-slate-200 dark:border-b-gray-800">
+      <div className="absolute left-0 top-1/2 transform -translate-y-1/2 ms-2 z-10">
+        <SidebarTrigger />
+      </div>
       <div className="max-w-11/12 lg:max-w-10/12 px-2 lg:px-4 mx-auto h-full flex justify-between items-center">
-        <div className="flex items-center ">
-          <SidebarTrigger />
+        <div className="flex items-center">
           <BrandLogo
             short
             className="h-5 w-max align-middle text-foreground select-none pointer-events-none hidden md:block"
